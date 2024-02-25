a94930c65f874c15be501baf5a202f438162ac73
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2024-02-25 19:19:55 +0530

    Fresh start

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,125 @@
-import 'dart:io';
-
-import 'package:file_manager/providers/database_provider.dart';
-import 'package:file_manager/utils.dart';
-import 'package:file_manager/database/db.dart';
-import 'package:file_manager/files/scanner.dart';
-import 'package:file_manager/providers/settings_provider.dart';
-import 'package:file_manager/providers/title_provider.dart';
-import 'package:file_manager/router/router.dart';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
-import 'package:provider/provider.dart';
 
-void main() async {
-  // debugPrint(
-  //   // "Using SQLite version ${sqlite3.version} from ${Uri.parse(Platform.resolvedExecutable).pathSegments.join(' | ')}",
-  //   "Using SQLite version ${sqlite3.version} from ${Uri.parse(Platform.resolvedExecutable).scheme}",
-  // );
-  // DB db = DB();
-  // debugPrint("Creating database at ${db.dbPath}");
-
-  // Scanner scanner = Scanner(
-  //     "C:/Users/jeffb/Desktop/dev/flutter/file_manager/build/windows/x64/runner/Debug");
-
-  // List<FileSystemEntity> list = await scanner.scan();
-
-  // Iterable<String> names = list
-  //     // .where((element) => element is! Directory)
-  //     .map((e) => "[${e.runtimeType}] ${e.path}");
-
-  // debugPrint(names.join("\n"));
-
-  debugPrint(rootPrefix(Platform.resolvedExecutable));
-
-  try {
-    runApp(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider<TitleProvider>(create: (_) => TitleProvider()),
-          ChangeNotifierProvider<SettingsProvider>(
-            create: (_) => SettingsProvider(),
-            lazy: false,
-          ),
-          ChangeNotifierProvider<DatabaseProvider>(
-            create: (_) => DatabaseProvider(),
-          ),
-        ],
-        child: const MyApp(),
-      ),
-    );
-  } finally {
-    // db.dispose();
-  }
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Consumer<TitleProvider>(builder: (
-      BuildContext context,
-      TitleProvider titleProvider,
-      Widget? child,
-    ) {
-      return MaterialApp.router(
-        title: titleProvider.title,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.deepPurple,
-            brightness: Brightness.light,
-          ),
-          useMaterial3: true,
-          brightness: Brightness.light,
-        ),
-        darkTheme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.deepPurple,
-            brightness: Brightness.dark,
-          ),
-          useMaterial3: true,
-          brightness: Brightness.dark,
-        ),
-        routerConfig: goRouter,
-      );
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
     });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
