ee3538e61091525540db3e3f3fd7534936ab7c2a
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2024-03-04 20:29:59 +0530

    Fixed tests to check for version set

--- test/providers/file_database_provider_test.dart
+++ test/providers/file_database_provider_test.dart
@@ -4,28 +4,29 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:file_manager/providers/file_database_provider.dart';
-import 'package:path/path.dart';
+import 'package:path/path.dart' as p;
 import 'package:sqlite3/open.dart';
 import 'package:sqlite3/sqlite3.dart';
 
 DynamicLibrary _openOnWindows() {
   final scriptDir = File(Platform.script.toFilePath()).parent;
   final libraryNextToScript = File(
-    join(scriptDir.path, 'test', 'providers', 'sqlite3', 'sqlite3.dll'),
+    p.join(scriptDir.path, 'test', 'providers', 'sqlite3', 'sqlite3.dll'),
   );
-  print("Loading dll from ${libraryNextToScript.path}");
   return DynamicLibrary.open(libraryNextToScript.path);
 }
 
 void main() {
+  setUp(() {
+    open.overrideFor(
+      OperatingSystem.windows,
+      () => _openOnWindows(),
+    );
+  });
+
   group('Database Migration Queries', () {
     test('Zero-to-Hero Migration', () {
       // Tests if the query is able to create a database from scratch
-      print(Platform.executable);
-      open.overrideFor(
-        OperatingSystem.windows,
-        () => _openOnWindows(),
-      );
 
       Database database = sqlite3.openInMemory();
       database.userVersion = 0;
@@ -37,11 +38,14 @@ void main() {
         while (migrationSql.containsKey(i)) {
           // Putting a null check here as this is checked in the parent `if` statement
           for (String sql in migrationSql[i]!) {
-            print("Executing:\n$sql");
+            // print("Executing:\n$sql");
             database.execute(sql);
           }
           i++;
         }
+        // The key is the version that the database is migrating from
+        // So the current version should be the next version
+        expect(i, equals(FileDatabaseProvider.dbMigrationSql.keys.last + 1));
       } finally {
         database.dispose();
       }
