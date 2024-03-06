8aa10f9397b30fa27440a6a852780479992eb08b
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2024-03-06 10:08:11 +0530

    Added methods to calculate file SHA1 hash and mimetype

--- lib/utils/database_file.dart
+++ lib/utils/database_file.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:convert/convert.dart';
+import 'package:crypto/crypto.dart';
+import 'package:mime/mime.dart';
 import 'package:path/path.dart';
 
 class DatabaseFile {
@@ -16,6 +20,12 @@ class DatabaseFile {
   ///
   /// Drive letters for all files except the executable drive are recorded.
   final String? driveRoot;
+
+  /// The mime-type of the file
+  ///
+  /// This is determined by using the filename and the first 256 bytes of the file contents.
+  ///
+  /// This is null if the mime-type could not be determined.
   final String mimeType;
   final String hash;
 
@@ -34,5 +44,43 @@ class DatabaseFile {
     } else {
       return fileRoot;
     }
+  }
+
+  /// Attempts to get the mime-type of the file.
+  ///
+  /// Uses the filename and the first [defaultMagicNumbersMaxLength] bytes to make the determination.
+  static Future<String?> getFileMimeType(String fullPath) async {
+    File file = File(fullPath);
+
+    Stream<List<int>> byteStream =
+        file.openRead(0, defaultMagicNumbersMaxLength);
+
+    List<int> bytes = (await byteStream.toList()).first;
+
+    return lookupMimeType(fullPath, headerBytes: bytes);
+  }
+
+  /// Gets the SHA1 hash of the file.
+  ///
+  /// SHA1 was chosen because it is reasonably fast and has reasonable resistance to hash collisions.
+  ///
+  /// Security concerns are ignored here as this is not a security-sensitive context.
+  static Future<String> getFileHash(String fullPath) async {
+    File file = File(fullPath);
+
+    AccumulatorSink<Digest> output = AccumulatorSink<Digest>();
+    ByteConversionSink input = sha1.startChunkedConversion(output);
+
+    Stream<List<int>> byteStream = file.openRead();
+
+    await for (final List<int> chunk in byteStream) {
+      input.add(chunk);
+    }
+
+    input.close();
+
+    Digest digest = output.events.single;
+
+    return digest.toString();
   }
 }
