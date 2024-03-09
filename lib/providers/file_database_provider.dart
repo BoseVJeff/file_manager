7d8ce536ca2a7b9c53642abf58a2141be9ac395a
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2024-03-09 21:24:06 +0530

    Mega commit adding various features to `FileDatabaseProvider`.
    `_filesToBeScanned` is now a list of tuples, relating each file with the id that its supposed to be in.
    
    Added `addsourcePath` to add source paths to `tbl_path` in the database
    
    Various methods have been modified to account for the above two changes
    
    Database has been modified so the database migration now has more queries.
    
    Database is now injected into the provider rather than opening it another time.
    
    Added implementation for `addScannedFilesToDatabase`.
    
    Database migration now treats negative versions as zero.

--- lib/providers/file_database_provider.dart
+++ lib/providers/file_database_provider.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:ffi';
 import 'dart:io';
 
+import 'package:file_manager/utils/database_file.dart';
 import 'package:flutter/foundation.dart';
 import 'package:logging/logging.dart';
 import 'package:sqlite3/sqlite3.dart';
@@ -23,7 +25,7 @@ class FileDatabaseProvider extends ChangeNotifier {
   ///
   /// This is a seperate list as the file scanning happens in a seperate isolate where the database object is unavailable.
   /// For more info on the scanning object see [FileDatabaseProvider.scanForFilesCompute].
-  final List<FileSystemEntity> _filesToBeScanned = [];
+  final List<(FileSystemEntity entity, int pathId)> _filesToBeScanned = [];
 
   /// Statements to migrate DB version `i` to DB version `i+1`.
   ///
@@ -37,6 +39,7 @@ class FileDatabaseProvider extends ChangeNotifier {
   /// Ideally, these statements are append-only and not modified once defined/released.
   // late Map<int, List<PreparedStatement>> _dbMigrationStmts;
   static final Map<int, List<String>> dbMigrationSql = {
+    // Initial table
     0: [
       """
 CREATE TABLE IF NOT EXISTS tbl_path (
@@ -56,6 +59,24 @@ CREATE TABLE IF NOT EXISTS tbl_file (
 );
 """
     ],
+    // `path_scanned_on_date` should really be `path_added_on_date` as ot records the timestamp when the path was added.
+    // Adding back a `path_scanned_on_date` with better constraints as that is also needed.
+    1: [
+      """
+ALTER TABLE tbl_path RENAME COLUMN path_scanned_on_date TO path_added_on_date;
+""",
+      """
+ALTER TABLE tbl_path ADD COLUMN path_scanned_on_date TEXT;
+""",
+    ],
+    // Database hashes are now strictly in uppercase
+    // Older versions of the code relied on the default dart behaviour, which would emit lowercase.
+    // The newer versions force uppercase, so the existing values must be updated so that hash equality continues to function as expected
+    2: [
+      """
+UPDATE tbl_file SET file_hash=upper(file_hash);
+"""
+    ]
   };
 
   // late PreparedStatement _databaseInsertFileStatement;
@@ -64,7 +85,7 @@ CREATE TABLE IF NOT EXISTS tbl_file (
 
   // late PreparedStatement _databaseFileDumpStatement;
 
-  // late PreparedStatement _databaseInsertScanPathStatement;
+  late PreparedStatement _databaseInsertScanPathStatement;
 
   // // TODO: Count the rows for each path_id in tbl_files
   // late PreparedStatement _databaseCountFilesFromPath;
@@ -77,25 +98,32 @@ CREATE TABLE IF NOT EXISTS tbl_file (
 
   // late PreparedStatement _databaseDeletePathChildrenStatement;
 
-  FileDatabaseProvider([String? databasePath])
-      : _database = (databasePath != null)
-            ? sqlite3.open(databasePath)
-            : sqlite3.openInMemory() {
+  late PreparedStatement _databaseLastPathIdStatement;
+
+  FileDatabaseProvider([Database? database])
+      : _database = database ?? sqlite3.openInMemory() {
     _logger.info(
-        "Initialising database ${(databasePath == null) ? 'in memory' : 'at $databasePath'}");
+        "Initialising database ${(database == null) ? 'in memory' : 'provided'}");
     _logger.info("Present database version: ${_database.userVersion}");
 
     _logger
         .config("Migrating Database from ${_database.userVersion} to latest");
     migrateDbToLatest();
+
+    _logger.fine("Preparing path insertion statement");
+    _databaseInsertScanPathStatement =
+        _database.prepare("INSERT INTO tbl_path (path) VALUES (?)");
+
+    _databaseLastPathIdStatement = _database
+        .prepare("SELECT path_id FROM tbl_path ORDER BY path_id DESC LIMIT 1;");
   }
 
-  // This method is mainly used for
+  // This method is mainly used for debugging and testing purposes
   int get databaseVersion => _database.userVersion;
 
   /// This migrates the existing DB to the current version and then sets the current version to the version it was updated to.
   ///
-  /// Does nothing if `currentVersion` is negative.
+  /// If [Database.userVersion] of the database is negative, it sets it to zero and proceeds assuming it is zero.
   ///
   /// Version `0` is considered to be non-existent.
   /// We delete the tables regardless if that is the case as an incorrect migration in the past may have screwed things up.
@@ -104,10 +132,16 @@ CREATE TABLE IF NOT EXISTS tbl_file (
   ///
   /// This is janky. May fix in the future.
   void migrateDbToLatest() {
+    if (_database.userVersion < 0) {
+      _database.userVersion = 0;
+    }
+    assert(_database.userVersion >= 0);
     int currentVersion = _database.userVersion;
     // Current version == 0 ==> The database does not exist
     if (currentVersion == 0) {
-      // Ensure that this always accounts for ALL tables involved.
+      // The database may have been created by another application or a much older version of the application.
+      // Deleting the tables involved ensures that we always start from scratch as this is unknown territory.
+      // NOTE: Ensure that this always accounts for ALL tables involved.
       _database.execute("DROP TABLE IF EXISTS tbl_file");
       _database.execute("DROP TABLE IF EXISTS tbl_path");
     }
@@ -124,7 +158,30 @@ CREATE TABLE IF NOT EXISTS tbl_file (
     _database.userVersion = i;
   }
 
+  int addSourcePath(String path) {
+    _logger.info("Adding path to tbl_path");
+    _databaseInsertScanPathStatement.execute([path]);
+
+    _logger.info("Returning the last row");
+    return _databaseLastPathIdStatement.select([]).first['path_id'];
+  }
+
+  /// Scans the given `path` and its subdirectories for files that can be added to the database.
+  /// These files are added to the an internal list of files that are pending to be scanned.
+  ///
+  /// Internally, this data is stored as a list of ([FileSystemEntity] entity, [int] pathId) tuples.
+  ///
+  /// Note that the file data is not computed untill just before the files are added to the database.
+  ///
+  /// This method spawns a seperate isolate for scanning, keeping the load off the main isolate.
+  ///
+  /// The path is also added into the database as a source path.
+  /// Specifically, the path is added to the `tbl_path` table in the database.
+  ///
+  /// This method does not check for duplicates, so it is the responsiblity of the caller to ensure that the same path does not get added twice.
   Future<void> scanForFilesCompute(String path) async {
+    int pathId = addSourcePath(path);
+
     _logger.info("Scanning $path for files");
 
     List<FileSystemEntity> tmpList =
@@ -170,7 +227,7 @@ CREATE TABLE IF NOT EXISTS tbl_file (
       debugLabel: "File Scan",
     );
 
-    _filesToBeScanned.addAll(tmpList);
+    _filesToBeScanned.addAll(tmpList.map((e) => (e, pathId)));
 
     _logger.info(
       "${_filesToBeScanned.length} entities awaiting addition to database",
@@ -178,6 +235,9 @@ CREATE TABLE IF NOT EXISTS tbl_file (
   }
 
   Future<void> scanForFiles(String path) async {
+    _logger.fine("Adding $path to database");
+    int pathId = addSourcePath(path);
+
     _logger.info("Scanning $path for files");
 
     switch (await FileSystemEntity.type(path)) {
@@ -188,12 +248,12 @@ CREATE TABLE IF NOT EXISTS tbl_file (
           followLinks: true,
         );
         await for (final FileSystemEntity entity in files) {
-          _filesToBeScanned.add(entity);
+          _filesToBeScanned.add((entity, pathId));
           // print(await entity.stat());
         }
         break;
       case FileSystemEntityType.file:
-        _filesToBeScanned.add(File(path));
+        _filesToBeScanned.add((File(path), pathId));
         break;
       case FileSystemEntityType.link:
         await scanForFiles(await Link(path).resolveSymbolicLinks());
@@ -213,7 +273,45 @@ CREATE TABLE IF NOT EXISTS tbl_file (
     );
   }
 
-  Future<void> addScannedFilesToDatabase() async {}
+  Future<void> addScannedFilesToDatabase() async {
+    // Handle to the database that will be passed into the isolate
+    final Pointer<void> databasePointer = _database.handle;
+
+    // Preparing file insert statement
+    // This is prepared here as there is an advantage to having this statement be pre-computed here for large number of files.
+    // Note that this statement MUST be disposed after it is done being used.
+    // PreparedStatement statement = _database.prepare(
+    //     "INSERT INTO tbl_file (file_drive_root, file_full_path, file_mime_type, file_hash) VALUES (?, ?, ?, ?);");
+
+    for (final (FileSystemEntity entity, int pathId) in _filesToBeScanned) {
+      await compute<(Pointer<void> pointer, String filePath, int pathid), void>(
+        (message) async {
+          Pointer<void> dbPointer;
+          String filePath;
+          int pathid;
+          (dbPointer, filePath, pathid) = message;
+
+          Database isolateDatabase = sqlite3.fromPointer(dbPointer);
+          DatabaseFile databaseFile = await DatabaseFile.fromPath(filePath);
+
+          isolateDatabase.execute(
+            "INSERT INTO tbl_file (file_drive_root, file_full_path, file_mime_type, file_hash, path_id) VALUES (?, ?, ?, ?, ?);",
+            [
+              databaseFile.driveRoot,
+              databaseFile.path,
+              databaseFile.mimeType,
+              databaseFile.hash,
+              pathid,
+            ],
+          );
+
+          return;
+        },
+        (databasePointer, entity.absolute.path, pathId),
+        debugLabel: 'File into Database',
+      );
+    }
+  }
 
   @override
   void dispose() {
